c93d1f7f9f14d7f0cfd7430307d6420ee8fd7306
Filipp Ivanov <feelout@ut.ee>
2017-03-07 12:36:30 +0200

    Updated to the latest plugin version + fixed some URLs

--- grails-app/conf/ApplicationResources.groovy
+++ grails-app/conf/ApplicationResources.groovy
@@ -4,9 +4,16 @@ modules = {
         resource url: [dir:'js', file:'application.js']
         resource url: [dir:'css', file:'generic.css']
     }
+
     bootstrap {
         resource url:[dir:'js', file:'bootstrap.js', plugin: 'collectory-plugin', disposition: 'head']
         resource url:[dir:'css', file:'bootstrap.css', plugin: 'collectory-plugin'], attrs:[media:'screen, projection, print']
         resource url:[dir:'css', file:'bootstrap-responsive.css', plugin: 'collectory-plugin'], attrs:[media:'screen', id:'responsiveCss']
+    }
+
+    charts {
+        dependsOn 'jquery_i18n'
+        resource url:[dir:'js', file:'charts2.js', plugin:'collectory-plugin']
+        resource url:[dir:'js', file:'charts.js']
     }
 }
