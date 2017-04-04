b27091407114ca43960f0fbe023abda362fc077a
Filipp Ivanov <feelout@ut.ee>
2017-04-04 10:29:56 +0300

    Import collectory.js

--- grails-app/conf/ApplicationResources.groovy
+++ grails-app/conf/ApplicationResources.groovy
@@ -1,4 +1,10 @@
 modules = {
+    collectory {
+        dependsOn 'jquery_ui_custom,smoothness,jquery_i18n,jquery_json,jquery_tools,jquery_jsonp,fancybox,openlayers,map'
+        resource url:[dir:'js', file:'collectory.js']
+        resource url:[dir:'css', file:'temp-style.css', plugin:'collectory-plugin']
+    }
+
     generic {
         dependsOn 'bootstrap, collectory' //
         resource url: [dir: 'js', file: 'application.js']
