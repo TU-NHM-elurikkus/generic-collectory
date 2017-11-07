65ad1a8b0c7ff371d9fc08e1ff8c1d10f25bf465
timo-piirmann <timurlane5@gmail.com>
2017-11-07 11:54:17 +0200

    Db confs (#71)

--- grails-app/conf/DataSource.groovy
+++ grails-app/conf/DataSource.groovy
@@ -1,35 +1,29 @@
 dataSource {
     pooled = true
     driverClassName = "com.mysql.jdbc.Driver"
+    dialect = org.hibernate.dialect.MySQL5InnoDBDialect
     username = "root"
     password = ""
     logSql = false
+    dbCreate = "update"
+    url = "jdbc:mysql://localhost:3306/collectory?autoReconnect=true&connectTimeout=0"
 }
+
 hibernate {
-    cache.use_second_level_cache=true
-    cache.use_query_cache=true
-    cache.provider_class='net.sf.ehcache.hibernate.EhCacheProvider'
+    cache.use_second_level_cache = true
+    cache.use_query_cache = true
+    cache.provider_class = "net.sf.ehcache.hibernate.EhCacheProvider"
 }
+
 // environment specific settings
 environments {
     development {
         dataSource {
-            dialect = org.hibernate.dialect.MySQL5InnoDBDialect
-            dbCreate = "update" // one of 'create', 'create-drop','update'
-            url = "jdbc:mysql://localhost:3306/collectory?autoReconnect=true&connectTimeout=0"
-            username = "root"
-            password = ""
-            //logSql = true
+
         }
     }
     test {
         dataSource {
-            dialect = org.hibernate.dialect.MySQL5InnoDBDialect
-            dbCreate = "update"
-            url = "jdbc:mysql://localhost:3306/collectory?autoReconnect=true&connectTimeout=0"
-            logSql = false
-            username = "root"
-            password = ""
             properties {
                 maxActive = 50
                 maxIdle = 25
@@ -44,10 +38,6 @@ environments {
     }
     production {
         dataSource {
-            dialect = org.hibernate.dialect.MySQL5InnoDBDialect
-            dbCreate = "update"
-            url = "jdbc:mysql://alaproddb1-cbr.vm.csiro.au:3306/collectory?autoReconnect=true&connectTimeout=0"
-            logSql = false
             properties {
                 maxActive = 50
                 maxIdle = 25
