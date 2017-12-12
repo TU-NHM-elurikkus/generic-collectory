4f09548527a8a03f47b4e70d46452b673e93235a
timo-piirmann <timurlane5@gmail.com>
2017-12-12 14:08:49 +0200

    JDBC confs

--- grails-app/conf/DataSource.groovy
+++ grails-app/conf/DataSource.groovy
@@ -6,20 +6,36 @@ dataSource {
     password = ""
     logSql = false
     dbCreate = "update"
-    url = "jdbc:mysql://localhost:3306/collectory?autoReconnect=true&connectTimeout=0"
+    url = "jdbc:mysql://localhost:3306/collectory"
     properties {
-        maxActive = 50
-        maxIdle = 25
-        minIdle = 5
+        jmxEnabled = true
         initialSize = 5
-        minEvictableIdleTimeMillis = 60000
-        timeBetweenEvictionRunsMillis = 60000
+        maxActive = 50
+        minIdle = 5
+        maxIdle = 25
         maxWait = 10000
-
+        maxAge = 10 * 60000
+        timeBetweenEvictionRunsMillis = 5000
+        minEvictableIdleTimeMillis = 60000
         validationQuery = "/* ping */"  // Better than "SELECT 1"
+        validationQueryTimeout = 3
+        validationInterval = 15000
         testOnBorrow = true
-        testOnReturn = true
         testWhileIdle = true
+        testOnReturn = false
+        jdbcInterceptors = "ConnectionState;StatementCache(max=200)"
+        defaultTransactionIsolation = java.sql.Connection.TRANSACTION_READ_COMMITTED
+        ignoreExceptionOnPreLoad = true
+
+        // controls for leaked connections
+        abandonWhenPercentageFull = 100 // settings are active only when pool is full
+        removeAbandonedTimeout = 120
+        removeAbandoned = true
+
+        dbProperties {
+            autoReconnect = false
+            connectTimeout = 60000
+        }
     }
 }
 
